9d6dc09605b0373e0a05fff5d7f9cf1f30d350e1
Bhadresh Desai <bhadresh_desai@hotmail.com>
2017-11-29 20:22:30 +0000

    Add unit tests

--- src/main/java/bdd/demo/springbootcxf/controller/WeatherServiceController.java
+++ src/main/java/bdd/demo/springbootcxf/controller/WeatherServiceController.java
@@ -2,14 +2,32 @@ package bdd.demo.springbootcxf.controller;
 
 import bdd.weather.weatherdatatype.WeatherRequest;
 import bdd.weather.weatherdatatype.WeatherResponse;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 public class WeatherServiceController {
+    private static final Map<String, String> CITY = createMap();
+    private static Map<String, String> createMap() {
+        Map<String, String> myMap = new HashMap<String, String>();
+        myMap.put("Mumbai", "Weather in Mumbai is fine");
+        myMap.put("Delhi", "Weather in Delhi is cold");
+        return myMap;
+    }
 
+    /*
+    private static final Map<Integer, String> myMap = Stream.of(
+            new AbstractMap.SimpleEntry<>(1, "one"),
+            new AbstractMap.SimpleEntry<>(2, "two"))
+            .collect(Collectors.toMap((entry) -> entry.getKey(), (entry) -> entry.getValue()));
+    */
+    
     public WeatherResponse getWeatherInformation(WeatherRequest parameters) {
         WeatherResponse response = new WeatherResponse();
         response.setReturnCode(1);
-        response.setReturnText("Weather in " + parameters.getZIP() + " is fine");
+        String text = Optional.ofNullable(CITY.get(parameters.getZIP())).orElse("Unknown city");
+        //response.setReturnText("Weather in " + parameters.getZIP() + " is fine");
+        response.setReturnText(text);
         return response;
     }
-    
 }
--- src/test/java/bdd/demo/springbootcxf/SpringBootCxfApplicationTests.java
+++ src/test/java/bdd/demo/springbootcxf/SpringBootCxfApplicationTests.java
@@ -1,16 +1,57 @@
 package bdd.demo.springbootcxf;
 
+import bdd.demo.springbootcxf.configuration.WebServiceConfiguration;
+import bdd.weather.weatherdatatype.WeatherRequest;
+import bdd.weather.weatherdatatype.WeatherResponse;
+import bdd.weather.weatherservice.GetWeatherInformationFault;
+import bdd.weather.weatherservice.WeatherService;
+import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBootCxfApplicationTests {
+    
+    public WeatherService getWeatherService() throws GetWeatherInformationFault {
+        JaxWsProxyFactoryBean jaxWsProxyFactory = new JaxWsProxyFactoryBean();
+        jaxWsProxyFactory.setServiceClass(WeatherService.class);
+        jaxWsProxyFactory.setAddress("http://localhost:8080" + WebServiceConfiguration.BASE_URL + WebServiceConfiguration.SERVICE_URL);
+        return (WeatherService) jaxWsProxyFactory.create();
+    }
 
     @Test
-    public void contextLoads() {
+    public void weatherInfoMumbai() throws GetWeatherInformationFault {
+        String city = "Mumbai";
+        WeatherService service = getWeatherService();
+        WeatherRequest parameters = new WeatherRequest();
+        parameters.setZIP(city);
+        WeatherResponse response = service.getWeatherInformation(parameters);
+        Assert.assertEquals("Weather in Mumbai is fine", response.getReturnText());
+    }
+    
+    @Test
+    public void weatherInfoDelhi() throws GetWeatherInformationFault {
+        String city = "Delhi";
+        WeatherService service = getWeatherService();
+        WeatherRequest parameters = new WeatherRequest();
+        parameters.setZIP(city);
+        WeatherResponse response = service.getWeatherInformation(parameters);
+        Assert.assertEquals("Weather in Delhi is cold", response.getReturnText());
+    }
+    
+    @Test
+    public void weatherInfoTimbakTo() throws GetWeatherInformationFault {
+        String city = "TimbakTo";
+        WeatherService service = getWeatherService();
+        WeatherRequest parameters = new WeatherRequest();
+        parameters.setZIP(city);
+        WeatherResponse response = service.getWeatherInformation(parameters);
+        Assert.assertEquals("Unknown city", response.getReturnText());
     }
 
 }
